3cd0f00ab06a669ea8778816b71e22d93c991fe9
Jared De Blander <jared@ayrscott.com>
2025-02-21 16:50:42 -0500

    raylib installed and working

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+from pyray import *
+
+
 def main():
     print("Hello from pyjamatime64!")
+
+    init_window(800, 450, "Hello")
+    while not window_should_close():
+        begin_drawing()
+        clear_background(WHITE)
+        draw_text("Hello world", 190, 200, 20, VIOLET)
+        end_drawing()
+    close_window()
 
 
 if __name__ == "__main__":
