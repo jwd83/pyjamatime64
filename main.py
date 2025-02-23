3f374c7b77121966e108b82ae180d6d8fc3e9ff5
Jared De Blander <jared@ayrscott.com>
2025-02-23 00:17:08 -0500

    main file shows runtime stats

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from classes import *
+import time
 
 
 def main():
-    print("PyjamaTime64: Starting up...")
-
-    Game().run()
+    start_time = time.time()
+    print(f"starting main:Game().run() at {start_time}")
+    g = Game()
+    g.run()
+    end_time = time.time()
+    print(f"finished main:Game().run() at {end_time}")
+    print(f"summary:")
+    print(f"last fps: {g.fps}")
+    print(f"frames: {g.frame}")
+    print(f"duration: {end_time - start_time}")
+    print(f"average fps: {g.frame / (end_time - start_time)}")
 
 
 if __name__ == "__main__":
