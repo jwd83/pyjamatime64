168c940aaeff21ecd99ebd642574446f165d1da7
Jared De Blander <jared@ayrscott.com>
2025-02-26 01:19:00 -0500

    sloan data updates

--- scenes/sloanviewer/sloanviewer.py
+++ scenes/sloanviewer/sloanviewer.py
@@ -3,22 +3,36 @@ from scipy.stats.kde import gaussian_kde
 import astropy.coordinates
 import astropy.units as u
 import numpy as np
+import matplotlib.pyplot as plt
 
-data = np.genfromtxt('your_csv_file'), delimiter=',')
-sdss_low_redshift = np.array([np.array([i[1], i[2], i[3]]) for i in data])
-comoving_dist = astropy.cosmology.WMAP9.comoving_distance(sdss_low_redshift[:, 2])
-c = astropy.coordinates.SkyCoord(ra=sdss_low_redshift[:, 0]*u.degree, dec=sdss_low_redshift[:, 1]*u.degree, distance=comoving_dist*u.mpc)
-sdss_pos = np.stack([np.array([i.x.value, i.y.value, i.z.value]) for i in c.cartesian])
+data = np.genfromtxt("sloandata.csv", delimiter=",")
+sloan_digital_sky_survey_low_redshift = np.array(
+    [np.array([i[1], i[2], i[3]]) for i in data]
+)
+comoving_dist = astropy.cosmology.WMAP9.comoving_distance(
+    sloan_digital_sky_survey_low_redshift[:, 2]
+)
+c = astropy.coordinates.SkyCoord(
+    ra=sloan_digital_sky_survey_low_redshift[:, 0] * u.degree,
+    dec=sloan_digital_sky_survey_low_redshift[:, 1] * u.degree,
+    distance=comoving_dist * u.mpc,
+)
+sloan_digital_sky_survey_pos = np.stack(
+    [np.array([i.x.value, i.y.value, i.z.value]) for i in c.cartesian]
+)
 # Removing nans
-mask = np.all(np.isnan(sdss_pos) | np.equal(sdss_pos, 0), axis=1)
-sdss_pos = sdss_pos[~mask]
+mask = np.all(
+    np.isnan(sloan_digital_sky_survey_pos) | np.equal(sloan_digital_sky_survey_pos, 0),
+    axis=1,
+)
+sloan_digital_sky_survey_pos = sloan_digital_sky_survey_pos[~mask]
 
 
-x, y = sdss_pos[:, 2], sdss_pos[:, 1]
+x, y = sloan_digital_sky_survey_pos[:, 2], sloan_digital_sky_survey_pos[:, 1]
 heatmap, xedges, yedges = np.histogram2d(x, y, bins=50)
 extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
 
 plt.clf()
 plt.figure(figsize=(8, 6))
-plt.gca().set_aspect('equal')
-plt.imshow(heatmap.T, extent=extent, origin='lower', cmap=plt.get_cmap('nipy_spectral'))
+plt.gca().set_aspect("equal")
+plt.imshow(heatmap.T, extent=extent, origin="lower", cmap=plt.get_cmap("nipy_spectral"))
