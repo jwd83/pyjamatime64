02259d4a6f46dafa08fa9ed5d912fc0d7dc4ca18
Jared De Blander <jared0x90@gmail.com>
2025-02-26 13:11:54 -0500

    zoom in with equal (plus) near backspace

--- scenes/sloanviewer/sloanviewer.py
+++ scenes/sloanviewer/sloanviewer.py
@@ -10,6 +10,9 @@ class SloanViewer(Scene):
     def __init__(self, game):
         super().__init__(game)
 
+        self.draw_max = 1
+        self.last_draw = 0
+
         self.star_positions = []
         path = os.path.join("scenes", "sloanviewer", "sloandata_out.csv")
 
@@ -32,9 +35,30 @@ class SloanViewer(Scene):
         # move the camera up and down vertical as well
         self.camera.position.y = math.sin(time.time() * 0.5) * 10
 
+        # check if enter key was just pressed
+        if is_key_pressed(KeyboardKey.KEY_ENTER):
+            self.draw_max *= 2
+
+        if is_key_pressed(KeyboardKey.KEY_EQUAL):
+            # increase every distance to origin by 33%
+            for pos in self.star_positions:
+                # each pos is a Vector3. increase it's distance to the origin
+                # by step_percent
+                step_percent = 1.10
+                pos.x *= step_percent
+                pos.y *= step_percent
+                pos.z *= step_percent
+
     def draw_3d(self):
+
+        draw_count = 0
         for pos in self.star_positions:
             draw_model(self.star_model, pos, 1.0, WHITE)
+            draw_count += 1
+            if draw_count > self.draw_max:
+                break
+
+        self.last_draw = draw_count
 
     def draw_2d(self):
         pass
