9c6925a9011f85241a9da71f4f499b162c5df8b9
Jared De Blander <jared0x90@gmail.com>
2025-03-02 17:22:40 -0500

    camera rotation

--- scenes/standardcandles/standardcandles.py
+++ scenes/standardcandles/standardcandles.py
@@ -53,8 +53,9 @@ camera around with free roam mouse steer and WASD
 """
 
 from pt import *
-
+import math
 import os
+import time
 
 
 class StandardCandles(Scene):
@@ -65,7 +66,9 @@ class StandardCandles(Scene):
         print("StandardCandles __init__ complete")
 
         # make a sphere mesh to hold the cmbr texture
-        self.cmbr_mesh = gen_mesh_sphere(5, 360, 360)
+        size = 3
+        rings_and_slices = 200
+        self.cmbr_mesh = gen_mesh_sphere(size, rings_and_slices, rings_and_slices)
         self.cmbr_model = load_model_from_mesh(self.cmbr_mesh)
         self.cmbr_texture = load_texture(
             os.path.join("assets", "textures", "wmap-sphere.png")
@@ -75,9 +78,30 @@ class StandardCandles(Scene):
         )
         self.cmbr_position = Vector3(0, 0, 0)
         self.cmbr_scale = 1
+        self.target = Vector3()
+        self.swap_center = True
+        self.center = Vector3()
 
     def update(self):
-        pass
+
+        # rotate camera around the origin by time
+        self.target.x = math.sin(time.time()) * 10
+        self.target.z = math.cos(time.time()) * 10
+
+        if self.swap_center:
+            self.camera.position = self.target
+            self.camera.target = self.center
+        else:
+            self.camera.position = self.center
+            self.camera.target = self.target
+        # move the camera up and down vertical as well
+
+        # self.camera.position.y = math.sin(time.time() * 0.5) * 10
+
+        # sort star positions by distance to camera
+
+        if is_key_pressed(KeyboardKey.KEY_C):
+            self.swap_center = not self.swap_center
 
     def draw_3d(self):
         draw_model(
