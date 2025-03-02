189e0f3e45e6187c8e8b432aa8ec31e9165a7b07
Jared De Blander <jared@ayrscott.com>
2025-03-02 14:04:05 -0500

    notes on standard candles viewer

--- scenes/standardcandles/standardcandles.py
+++ scenes/standardcandles/standardcandles.py
@@ -8,6 +8,38 @@ can specify linear, logarithmic, or exponential scales for the distances to crus
 relatively closer into the scene. The user can also specify the color of the stars, and the
 size of the stars.
 
+Data sets:
+    Nearby:
+        Parallax
+        Exoplanets
+        constellations
+        solar system
+        milky way
+        messier objects
+    Distant:
+        Cepheid variables
+        Type 1a supernovae
+        hubble deep field
+        CMBR bubble
+        
+    Black holes
+
+
+Viewing modes:
+    Orbiting earth.
+    Viewing from earth.
+    Free roam
+
+Distance scales:
+    Linear
+    Logarithmic
+    Exponential
+
+Mouseover:
+    Show star name
+    Show star distance
+    Show star size
+    Show star color
 
 
 
