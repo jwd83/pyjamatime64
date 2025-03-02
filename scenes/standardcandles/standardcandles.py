edb38072a9f9688a62ce8cb679e034600bec0ac8
Jared De Blander <jared@ayrscott.com>
2025-03-02 13:55:45 -0500

    starting a standard candles scene

---
+++ scenes/standardcandles/standardcandles.py
@@ -0,0 +1,22 @@
+from pt import *
+
+
+class StandardCandles(Scene):
+
+    def __init__(self, game):
+        super().__init__(game)
+
+        print("StandardCandles __init__ complete")
+
+    def __str__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def draw_3d(self):
+
+        pass
+
+    def draw_2d(self):
+        pass
