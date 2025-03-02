139ea7d7339a5da98f47ff34d965cc9d0ab8073f
Jared De Blander <jared0x90@gmail.com>
2025-03-01 21:45:34 -0500

    more data viewer updates

--- scenes/sloanviewer/sloanviewer.py
+++ scenes/sloanviewer/sloanviewer.py
@@ -15,7 +15,7 @@ class SloanViewer(Scene):
 
         self.star_positions = []
         # path = os.path.join("scenes", "sloanviewer", "sloandata_out.csv")
-        path = os.path.join("scenes", "sloanviewer", "sloandata2_out_clean.csv")
+        path = os.path.join("scenes", "sloanviewer", "sloan-nearby-redshift-out.csv")
 
         self.star_mesh = gen_mesh_sphere(0.01, 10, 10)
         self.star_model = load_model_from_mesh(self.star_mesh)
@@ -29,6 +29,10 @@ class SloanViewer(Scene):
 
         self.star_positions.sort(key=lambda x: x.x * x.x + x.y * x.y + x.z * x.z)
 
+        self.target = Vector3()
+        self.center = Vector3()
+        self.swap_center = False
+
     def update(self):
 
         self.debug.append(f"Loaded objects: {len(self.star_positions)}")
@@ -36,13 +40,23 @@ class SloanViewer(Scene):
         # self.debug.append(f"Draw limit: {self.draw_max}")
 
         # rotate camera around the origin by time
-        self.camera.position.x = math.sin(time.time()) * 10
-        self.camera.position.z = math.cos(time.time()) * 10
+        self.target.x = math.sin(time.time()) * 10
+        self.target.z = math.cos(time.time()) * 10
 
+        if self.swap_center:
+            self.camera.position = self.target
+            self.camera.target = self.center
+        else:
+            self.camera.position = self.center
+            self.camera.target = self.target
         # move the camera up and down vertical as well
+
         # self.camera.position.y = math.sin(time.time() * 0.5) * 10
 
         # sort star positions by distance to camera
+
+        if is_key_pressed(KeyboardKey.KEY_C):
+            self.swap_center = not self.swap_center
 
         # check if enter key was just pressed
         if is_key_pressed(KeyboardKey.KEY_ENTER):
@@ -62,12 +76,13 @@ class SloanViewer(Scene):
 
         draw_count = 0
         for pos in self.star_positions:
-            draw_model(self.star_model, pos, 1.0, WHITE)
+            draw_model(self.star_model, pos, 1, WHITE)
             draw_count += 1
             if draw_count > self.draw_max:
                 break
 
-        draw_model(self.star_model, Vector3(), 1.0, BLUE)
+        if self.swap_center:
+            draw_model(self.star_model, Vector3(), 1.0, BLUE)
 
         self.last_draw = draw_count
 
