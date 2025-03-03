cb64f3ac50253f1d07be61445d9cb024acb40e10
Jared De Blander <jared0x90@gmail.com>
2025-03-03 17:47:02 -0500

    earth inside CMBR

--- scenes/__init__.py
+++ scenes/__init__.py
@@ -4,3 +4,4 @@ from .sloanviewer.sloanviewer import SloanViewer
 from .standardcandles.standardcandles import StandardCandles
 from .cmbrdemo import CMBRDemo
 from .earthdemo import EarthDemo
+from .globesdemo import GlobesDemo
