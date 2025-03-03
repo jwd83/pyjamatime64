461eddfa2eea697d3a6657d1b83d4f69ad657e86
Jared De Blander <jared0x90@gmail.com>
2025-03-03 17:50:08 -0500

    toggle CMBR and earth with C and E

--- scenes/globesdemo.py
+++ scenes/globesdemo.py
@@ -11,13 +11,16 @@ class GlobesDemo(Scene):
 
         self.earth = load_model(os.path.join("models", "earth.glb"))
         self.earth_position = Vector3(0, 0, 0)
-        self.earth_scale = 2.0
+        self.earth_scale = 1.0
         self.earth_color = WHITE
 
         self.cmbr = load_model(os.path.join("models", "cmbr4.glb"))
         self.cmbr_position = Vector3(0, 0, 0)
         self.cmbr_scale = 4.0
-        self.cmbr_color = Color(255, 255, 255, 128)
+        self.cmbr_color = Color(255, 255, 255, 96)
+
+        self.draw_cmbr = True
+        self.draw_earth = True
 
         print("GlobesDemo __init__ complete")
 
@@ -30,9 +33,19 @@ class GlobesDemo(Scene):
         # move the camera up and down vertical as well
         self.camera.position.y = math.sin(time.time() * 0.5) * 10
 
+        if is_key_pressed(rl.KEY_E):
+            self.draw_earth = not self.draw_earth
+
+        if is_key_pressed(rl.KEY_C):
+            self.draw_cmbr = not self.draw_cmbr
+
     def draw_3d(self):
-        draw_model(self.earth, self.earth_position, self.earth_scale, self.earth_color)
-        draw_model(self.cmbr, self.cmbr_position, self.cmbr_scale, self.cmbr_color)
+        if self.draw_earth:
+            draw_model(
+                self.earth, self.earth_position, self.earth_scale, self.earth_color
+            )
+        if self.draw_cmbr:
+            draw_model(self.cmbr, self.cmbr_position, self.cmbr_scale, self.cmbr_color)
 
     def draw_2d(self):
         pass
