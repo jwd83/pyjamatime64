fce94bc3925aac4c3469b695f545f8269fb6d3b8
Jared De Blander <jared0x90@gmail.com>
2025-03-05 14:30:56 -0500

    updates to globes demo

--- scenes/globesdemo.py
+++ scenes/globesdemo.py
@@ -11,12 +11,12 @@ class GlobesDemo(Scene):
 
         self.earth = load_model(os.path.join("models", "earth.glb"))
         self.earth_position = Vector3(0, 0, 0)
-        self.earth_scale = 1.0
+        self.earth_scale = 5.0
         self.earth_color = WHITE
 
         self.cmbr = load_model(os.path.join("models", "cmbr4.glb"))
         self.cmbr_position = Vector3(0, 0, 0)
-        self.cmbr_scale = 4.0
+        self.cmbr_scale = 0.1
         self.cmbr_color = Color(255, 255, 255, 96)
 
         self.draw_cmbr = True
@@ -24,7 +24,7 @@ class GlobesDemo(Scene):
 
         print("GlobesDemo __init__ complete")
 
-    def update(self):
+    def update_camera(self):
 
         # rotate camera around the origin by time
         self.camera.position.x = math.sin(time.time()) * 10
@@ -33,11 +33,20 @@ class GlobesDemo(Scene):
         # move the camera up and down vertical as well
         self.camera.position.y = math.sin(time.time() * 0.5) * 10
 
+    def update_inputs(self):
+
         if is_key_pressed(rl.KEY_E):
             self.draw_earth = not self.draw_earth
 
         if is_key_pressed(rl.KEY_C):
             self.draw_cmbr = not self.draw_cmbr
+
+    def update(self):
+        self.update_camera()
+        self.update_inputs()
+
+        self.earth_scale = max(0.05, self.earth_scale - self.game.dt)
+        self.cmbr_scale = min(4, self.cmbr_scale + self.game.dt)
 
     def draw_3d(self):
         if self.draw_earth:
