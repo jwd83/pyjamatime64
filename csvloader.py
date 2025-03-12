460bd1ad198829f49c2f81e9225d0d5dcc04d078
Jared De Blander <jared0x90@gmail.com>
2025-03-12 11:18:44 -0400

    improvements to csv loader

--- csvloader.py
+++ csvloader.py
@@ -5,31 +5,36 @@ import sys
 
 def main():
     # check if the argument is a file that exists
-    file_path = sys.argv[1]
+    path = sys.argv[1]
 
-    # check if the file exists
-    if not os.path.exists(file_path):
-        print(f"File {file_path} does not exist.")
-        sys.exit(1)
-
-    # check if the file is a file
-    if not os.path.isfile(file_path):
-        print(f"{file_path} is not a file.")
-        sys.exit(1)
-
-    rows = load_csv(file_path)
+    rows = load_csv(path)
     print(len(rows))
 
     for row in rows:
         print(row["name"])
 
 
-def load_csv(path: str):
+def load_csv(path: str, header_row: int = 1) -> list | bool:
+
     rows = []
 
-    # open the CSV as a DictReader with first row as keys
+    # check if the file exists
+    if not os.path.exists(path):
+        print(f"File {path} does not exist.")
+        return False
 
+    # check if the file is a file
+    if not os.path.isfile(path):
+        print(f"{path} is not a file.")
+        return False
+
+    # open the CSV as a DictReader
     with open(path, "r") as f:
+        # skip rows if header_row is greater than 1
+        if header_row > 1:
+            for _ in range(header_row - 1):
+                f.readline()
+
         reader = csv.DictReader(f)
         for row in reader:
             rows.append(row)
