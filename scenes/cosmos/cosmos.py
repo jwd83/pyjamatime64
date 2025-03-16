98edfdd5d1b47a98a39e405d8761f54906400ddb
Jared De Blander <jared@ayrscott.com>
2025-03-16 03:57:41 -0400

    drawing earth and sun

--- scenes/cosmos/cosmos.py
+++ scenes/cosmos/cosmos.py
@@ -33,10 +33,20 @@ class Cosmos(Scene):
         self.camera.position.z = math.cos(time.time()) * 10
 
     def draw_3d(self):
-        # draw the grid
-        draw_grid(10, 1)
-
+        self.draw_solar_system()
         pass
+
+    def draw_solar_system(self):
+
+        # draw the grid
+        draw_grid(100, 1)
+
+        # draw the sun
+        draw_model(self.models["sun.glb"], Vector3(0, 0, 0), 1.0, WHITE)
+
+        # draw the earth, moon and mars
+        
+        draw_model(self.models["earth.glb"], Vector3(5, 0, 0), 1.0, BLUE)
 
     def draw_2d(self):
         pass
