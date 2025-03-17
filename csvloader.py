6beb3ce75a95f8bf76d86656186f574efa83ae33
Jared De Blander <jared0x90@gmail.com>
2025-03-17 00:52:55 -0400

    solar system view in light minutes. sun draw to scale and earth drawn at scale distance.

---
+++ csvloader.py
@@ -0,0 +1,65 @@
+import csv
+import os
+import sys
+
+
+def main():
+    # check if the argument is a file that exists
+    path = sys.argv[1]
+
+    rows = load_csv(path)
+    print(len(rows))
+
+    for row in rows:
+        print(row["name"])
+
+
+def load_csv(path: str, header_row: int = 1, skip_comments: bool = True) -> list | bool:
+
+    rows = []
+
+    # check if the path exists
+    if not os.path.exists(path):
+        print(f"Error: Supplied path does not exist: {path}")
+        return False
+
+    # check if the path is a file
+    if not os.path.isfile(path):
+        print(f"Error: Supplied path is not a file: {path}")
+        return False
+
+    # open the CSV as a DictReader
+    with open(path, "r") as f:
+        # skip rows if header_row is greater than 1
+        if header_row > 1:
+            for _ in range(header_row - 1):
+                f.readline()
+
+        # Check for multiple comment lines
+        if skip_comments:
+            while True:
+                comment_check_position = f.tell()
+                comment_check = f.readline().strip()
+                if not comment_check.startswith("#"):
+                    f.seek(comment_check_position)
+                    break
+                else:
+                    print(f"Header comment: {comment_check}")
+
+        reader = csv.DictReader(f)
+        for row in reader:
+            add_row = True
+            for _, value in row.items():
+                first_item = value.strip()
+                if first_item.startswith("#"):
+                    add_row = False
+                    print(f"Data comment: {value}")
+                break
+            if add_row:
+                rows.append(row)
+
+    return rows
+
+
+if __name__ == "__main__":
+    main()
--- scenes/cosmos/cosmos.py
+++ scenes/cosmos/cosmos.py
@@ -3,13 +3,39 @@ import math
 import time
 
 
+def light_year_to_light_minute(light_year: float):
+    constant = 525960
+    return light_year * constant
+
+
+def miles_to_light_minute(miles: float):
+    constant = 8.9469895870429e-8
+    return miles * constant
+
+
 class Cosmos(Scene):
+
+    def prop_eq(self, property, value, limit_1: bool = False):
+        results = []
+        for row in self.data:
+            if row[property] == value:
+                if limit_1:
+                    return row
+                else:
+                    results.append(row)
+
+        return results
 
     def __init__(self, game):
         super().__init__(game)
 
         self.data = load_csv("assets/data/stellar-neighborhood.csv")
         self.models = {}
+        self.neighborhood = {
+            "earth": self.prop_eq("name", "Earth", True),
+            "mars": self.prop_eq("name", "Mars", True),
+            "sun": self.prop_eq("name", "Sun", True),
+        }
 
         for row in self.data:
             model = str(row["model"]).strip()
@@ -24,7 +50,22 @@ class Cosmos(Scene):
 
         print(f"{len(self.data)} records loaded")
 
+        # compute distances
+        self.precompute_data()
+
         print("Cosmos __init__ complete")
+
+    def precompute_data(self):
+        self.earth_light_minutes = light_year_to_light_minute(
+            float(self.neighborhood["earth"]["dist"])
+        )
+        self.earth_position_solar_system = Vector3(self.earth_light_minutes, 0, 0)
+
+        # the model default scale has radius 1, so diameter 2. so our diameter number
+        # needs to be divided by 2 to get the scale
+        self.sun_scale = (
+            miles_to_light_minute(float(self.neighborhood["sun"]["diameter"])) / 2
+        )
 
     def update(self):
 
@@ -34,7 +75,6 @@ class Cosmos(Scene):
 
     def draw_3d(self):
         self.draw_solar_system()
-        pass
 
     def draw_solar_system(self):
 
@@ -42,11 +82,19 @@ class Cosmos(Scene):
         draw_grid(100, 1)
 
         # draw the sun
-        draw_model(self.models["sun.glb"], Vector3(0, 0, 0), 1.0, WHITE)
+        draw_model(
+            self.models["sun.glb"],
+            Vector3(0, 0, 0),
+            self.sun_scale,
+            WHITE,
+        )
 
         # draw the earth, moon and mars
-        
-        draw_model(self.models["earth.glb"], Vector3(5, 0, 0), 1.0, BLUE)
+        print(light_year_to_light_minute(float(self.neighborhood["earth"]["dist"])))
+
+        draw_model(
+            self.models["earth.glb"], self.earth_position_solar_system, 1.0, WHITE
+        )
 
     def draw_2d(self):
         pass
