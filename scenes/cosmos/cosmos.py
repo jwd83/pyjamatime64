df10d443c2e163e661030ef569fe8933d59a8255
Jared De Blander <jared@ayrscott.com>
2025-03-17 23:52:19 -0400

    building out the solar system

--- scenes/cosmos/cosmos.py
+++ scenes/cosmos/cosmos.py
@@ -32,6 +32,7 @@ class Cosmos(Scene):
         self.data = load_csv("assets/data/stellar-neighborhood.csv")
         self.models = {}
         self.neighborhood = {
+            "moon": self.prop_eq("name", "Moon", True),
             "earth": self.prop_eq("name", "Earth", True),
             "mars": self.prop_eq("name", "Mars", True),
             "sun": self.prop_eq("name", "Sun", True),
@@ -58,13 +59,18 @@ class Cosmos(Scene):
         print("Cosmos __init__ complete")
 
     def precompute_data(self):
+
+        # the model default scale has radius 1, so diameter 2. so our diameter number
+        # needs to be divided by 2 to get the scale
         self.earth_light_minutes = light_year_to_light_minute(
             float(self.neighborhood["earth"]["dist"])
         )
         self.earth_position_solar_system = Vector3(self.earth_light_minutes, 0, 0)
-
-        # the model default scale has radius 1, so diameter 2. so our diameter number
-        # needs to be divided by 2 to get the scale
+        self.moon_light_minutes = light_year_to_light_minute(
+            float(self.neighborhood["mars"]["dist"])
+        )
+        self.moon_position_solar_system = Vector3(self.moon_light_minutes, 0, 0)
+        self.sun_position_solar_system = Vector3(0, 0, 0)
         self.sun_scale = (
             miles_to_light_minute(float(self.neighborhood["sun"]["diameter"])) / 2
         )
@@ -73,12 +79,16 @@ class Cosmos(Scene):
             miles_to_light_minute(float(self.neighborhood["earth"]["diameter"])) / 2
         )
 
+        self.moon_scale = (
+            miles_to_light_minute(float(self.neighborhood["moon"]["diameter"])) / 2
+        )
+
     def update(self):
 
         # rotate camera around the origin by time
         self.camera.position.x = math.sin(time.time()) * 10
         self.camera.position.z = math.cos(time.time()) * 10
-        self.scale_multiplier = min(1.0, math.sin(time.time() / 2) * 250)
+        self.scale_multiplier = min(1.0, math.sin(time.time() / 2) * 400)
 
     def draw_3d(self):
         self.draw_solar_system()
@@ -92,22 +102,31 @@ class Cosmos(Scene):
         draw_model(
             self.models["sun.glb"],
             Vector3(0, 0, 0),
-            self.sun_scale * self.scale_multiplier,
+            min(1.0, self.sun_scale * self.scale_multiplier),
             WHITE,
         )
-
-        print(light_year_to_light_minute(float(self.neighborhood["earth"]["dist"])))
 
         # draw the earth
         draw_model(
             self.models["earth.glb"],
             self.earth_position_solar_system,
-            self.earth_scale * self.scale_multiplier,
+            min(1.0, self.earth_scale * self.scale_multiplier * 20),
+            WHITE,
+        )
+
+        # draw the moon
+        draw_model(
+            self.models["moon.glb"],
+            self.moon_position_solar_system,
+            min(1.0, self.moon_scale * self.scale_multiplier * 20),
             WHITE,
         )
 
         # draw a blue line from the earth to the sun
         draw_line_3d(self.earth_position_solar_system, Vector3(0, 0, 0), BLUE)
+        draw_line_3d(
+            self.earth_position_solar_system, self.moon_position_solar_system, RED
+        )
 
     def draw_2d(self):
         pass
