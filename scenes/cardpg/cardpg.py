b624971418ace0a5ad91c3d9f57cb34ef6a76dbb
Jared De Blander <jared@ayrscott.com>
2025-03-22 17:58:59 -0400

    card stuffs

---
+++ scenes/cardpg/cardpg.py
@@ -0,0 +1,49 @@
+from pt import *
+
+"""
+
+
+# classes
+
+
+# buffs and debuffs falls off 1 stack per round
+
+FIGHTER
+cecil harvey
+kain highwind
+
+MAGE
+rydia
+tellah
+
+CLERIC
+rosa
+aerith
+yuna
+
+
+engineer
+cid highwind
+edge geraldine
+setzer
+
+
+"""
+
+
+class CardPG(Scene):
+
+    def __init__(self, game):
+        super().__init__(game)
+
+        print("CardPG __init__ complete")
+
+    def update(self):
+        pass
+
+    def draw_3d(self):
+
+        pass
+
+    def draw_2d(self):
+        pass
