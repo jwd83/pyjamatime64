e77538ad60e77ddbbfa745ad25edcd62c41eb2cb
Jared De Blander <jared@ayrscott.com>
2025-03-23 19:16:01 -0400

    new assets

--- scenes/dragster/dragster.py
+++ scenes/dragster/dragster.py
@@ -6,6 +6,8 @@ class Dragster(Scene):
     def __init__(self, game):
         super().__init__(game)
 
+        engines = pcsv.load("assets/data/torque-curves.csv")
+
         print("Dragster __init__ complete")
 
     def update(self):
