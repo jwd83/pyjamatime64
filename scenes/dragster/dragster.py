b564e9efb7cad4851afbdbe084afe41b386e18e1
Jared De Blander <jared@ayrscott.com>
2025-03-23 19:00:46 -0400

    dragster game

---
+++ scenes/dragster/dragster.py
@@ -0,0 +1,19 @@
+from pt import *
+
+
+class Dragster(Scene):
+
+    def __init__(self, game):
+        super().__init__(game)
+
+        print("Dragster __init__ complete")
+
+    def update(self):
+        pass
+
+    def draw_3d(self):
+
+        pass
+
+    def draw_2d(self):
+        pass
