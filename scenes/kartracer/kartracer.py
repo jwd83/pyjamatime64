17c94f1fa6870024c64e7663600cfd04e56befb3
Jared De Blander <jared@ayrscott.com>
2025-03-23 13:36:02 -0400

    kart racer updates

--- scenes/kartracer/kartracer.py
+++ scenes/kartracer/kartracer.py
@@ -8,6 +8,12 @@ class KartRacer(Scene):
 
         print("Kart __init__ complete")
 
+        self.tach_max_rpm = 8000
+        self.tach_redline = 7000
+        self.tach_rpm = 0
+        self.tach_start_angle = 0
+        self.tach_end_angle = 270
+
     def update(self):
         pass
 
@@ -16,4 +22,7 @@ class KartRacer(Scene):
         pass
 
     def draw_2d(self):
+        pass
+
+    def draw_tach(self):
         pass
